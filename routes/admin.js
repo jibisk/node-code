678acda43c22feb9e2c6a35271b72b9086ca1696
jibisk <jiby.elavinmootil@gmail.com>
2025-08-19 15:07:28 +0400

    first code commit

---
+++ routes/admin.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+router.get('/add-product', (req, res, next)=>{
+//   res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+//   res.sendFile(path.join( __dirname, '..', 'views', 'add-product.html' ))
+//   res.sendFile(path.join( rootDir, 'views', 'add-product.html' ))
+
+    // res.render('add-product', {docTitle:'Add A Product', path:'/admin/add-product'});
+
+    // res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true });
+
+    res.render('add-product', {pageTitle:'Add A Product', path:'/admin/add-product'});
+
+
+})
+
+router.post('/add-product', (req, res, next)=>{
+
+  products.push({title: req.body.title})
+
+  res.redirect('/');
+})
+
+// module.exports = router;
+
+exports.routers = router;
+exports.products = products;
